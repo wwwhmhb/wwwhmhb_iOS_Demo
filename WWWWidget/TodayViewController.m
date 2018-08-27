91fb4857e419090ab1398e9d42678bcf3dbd77f7
京城六少 <wwwhmhb@sina.com>
2018-08-27 23:09:37 +0800

    完善 widget, 展开折叠功能,去掉 mainInterface.StoryBoard

--- WWWWidget/TodayViewController.m
+++ WWWWidget/TodayViewController.m
@@ -19,11 +19,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 200, 30)];
-    label.backgroundColor = [UIColor yellowColor];
-    label.textColor = [UIColor blueColor];
-    label.text = @"Hello,Word!";
-    [self.view addSubview:label];
+    //iOS10之后，Widget支持展开及折叠两种展现方式
+    if (@available(iOS 10.0, *)) {
+        self.extensionContext.widgetLargestAvailableDisplayMode = NCWidgetDisplayModeExpanded;
+    }
+//
+//    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 200, 30)];
+//    label.backgroundColor = [UIColor yellowColor];
+//    label.textColor = [UIColor blueColor];
+//    label.text = @"Hello,Word!";
+//    [self.view addSubview:label];
     
 }
 
@@ -40,6 +45,22 @@
     // If there's an update, use NCUpdateResultNewData
 
     completionHandler(NCUpdateResultNewData);
+}
+
+//重写切换展开及折叠布局时的方法（iOS10之后）
+- (void)widgetActiveDisplayModeDidChange:(NCWidgetDisplayMode)activeDisplayMode withMaximumSize:(CGSize)maxSize API_AVAILABLE(ios(10.0)) {
+    NSLog(@"maxWidth %f maxHeight %f",maxSize.width,maxSize.height);
+    
+    if (activeDisplayMode == NCWidgetDisplayModeCompact) {
+        self.preferredContentSize = CGSizeMake(maxSize.width, 110);
+    } else {
+        self.preferredContentSize = CGSizeMake(maxSize.width, 200);
+    }
+}
+
+//iOS10之前，视图原点默认存在一个间距，可以实现以下方法来调整视图间距
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets {
+    return UIEdgeInsetsMake(0, 10, 0, 10);
 }
 
 @end
