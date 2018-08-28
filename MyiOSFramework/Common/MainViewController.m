d373292a2b05742b983696d50091394ff7ed23f8
Wangweiwei <wangweiwei@youerobot.com>
2018-08-28 18:34:27 +0800

    优化pragma

--- MyiOSFramework/Common/MainViewController.m
+++ MyiOSFramework/Common/MainViewController.m
@@ -16,31 +16,31 @@
 @end
 
 @implementation MainViewController
-#pragma mark - ---------- Lazy Loading（懒加载） ----------
 
-#pragma mark - ----------   Lifecycle（生命周期） ----------
 
-#pragma mark - ---------- Private Methods（私有方法） ----------
+#pragma mark -- self declare （本类声明）
 
-#pragma mark initliaze data(初始化数据)
+#pragma mark -- initliaze data(初始化数据)
 
-#pragma mark config control（布局控件）
+#pragma mark -- lifecycle（生命周期）
 
-#pragma mark networkRequest (网络请求)
+#pragma mark -- override super （重写父类）
 
-#pragma mark actions （点击事件）
+#pragma mark -- public Methods（公有方法）
 
-#pragma mark IBActions （点击事件xib）
+#pragma mark -- config control（布局控件）
 
-#pragma mark - ---------- Public Methods（公有方法） ----------
+#pragma mark -- actions （点击事件）
 
-#pragma mark self declare （本类声明）
+#pragma mark -- IBActions （点击事件xib）
 
-#pragma mark override super （重写父类）
+#pragma mark -- networkRequest (网络请求)
 
-#pragma mark setter （重写set方法）
+#pragma mark -- protocol Methods（代理方法）
 
-#pragma mark - ---------- Protocol Methods（代理方法） ----------
+#pragma mark -- setter getter （set和getter方法）
+
+#pragma mark -- private Methods（私有方法）
 
 - (void)viewDidLoad {
     [super viewDidLoad];
