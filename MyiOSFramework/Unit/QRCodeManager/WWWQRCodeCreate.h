f42ce90b55a3b6aa6c7671f994e13712a11dd150
Wangweiwei <wangweiwei@youerobot.com>
2018-09-21 14:13:58 +0800

    二维码扫描和生成的功能添加

---
+++ MyiOSFramework/Unit/QRCodeManager/WWWQRCodeCreate.h
@@ -0,0 +1,13 @@
+//
+//  WWWQRCodeCreate.h
+//  MyiOSFramework
+//
+//  Created by Yonggui Wang on 2018/9/21.
+//  Copyright © 2018年 Weiwei Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWWQRCodeCreate : NSObject
+
+@end
---
+++ MyiOSFramework/Unit/QRCodeManager/WWWQRCodeCreate.m
@@ -0,0 +1,13 @@
+//
+//  WWWQRCodeCreate.m
+//  MyiOSFramework
+//
+//  Created by Yonggui Wang on 2018/9/21.
+//  Copyright © 2018年 Weiwei Wang. All rights reserved.
+//
+
+#import "WWWQRCodeCreate.h"
+
+@implementation WWWQRCodeCreate
+
+@end
